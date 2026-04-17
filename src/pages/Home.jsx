f2af294f03f81ddaa68e014faf8ecac3185dd3da
isabel64450 <isa.dumas64@gmail.com>
2026-04-17 15:24:35 +0200

    burger menu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-     <div className="relative z-10 flex flex-col items-center justify-start min-h-screen pt-28 text-white">
+     <div className="relative z-10 flex flex-col items-center justify-start min-h-screen pt-24 md:pt-28 text-white">
 
       <img
         src={Galop}
@@ -15,7 +15,7 @@ export default function Home() {
    
       <div className="relative z-10 text-white">
 
-        <h1 className="text-5xl font-bold">
+        <h1 className="text-3xl md:text-5x1">
           Izaberu Creations
         </h1>
 
@@ -23,16 +23,16 @@ export default function Home() {
           Watercolors & Handmade Jewelry
         </p>
 
-        <div className="flex gap-8 justify-center">
+        <div className="flex flex-col md:flex-row gap-6 md:gap-8 items-center justify-center">
 
          
-          <Link to="/gallery" className="w-60 h-72 rounded-2xl border border-white/20 bg-white/10 backdrop-blur-xl flex flex-col justify-center items-center transition hover:-translate-y-2 hover:bg-[#84a98c]/30 cursor-pointer">
+          <Link to="/gallery" className="w-40 h-56 md:w-60 md:h-72 rounded-2xl border border-white/20 bg-white/10 backdrop-blur-xl flex flex-col justify-center items-center transition hover:-translate-y-2 hover:bg-[#84a98c]/30 cursor-pointer">
              <h2 className="text-xl font-semibold">Warcolors</h2>
                <p className="text-sm opacity-80">Watercolor art</p>
           </Link>
 
           
-          <Link to="/" className="w-60 h-72 rounded-2xl border border-white/20 bg-white/10 backdrop-blur-xl flex flex-col justify-center items-center transition hover:-translate-y-2 hover:bg-[#52796f]/30 cursor-pointer">
+          <Link to="/" className="w-40 h-56 md:w-60 md:h-72 rounded-2xl border border-white/20 bg-white/10 backdrop-blur-xl flex flex-col justify-center items-center transition hover:-translate-y-2 hover:bg-[#52796f]/30 cursor-pointer">
             <h2 className="text-xl font-semibold">Bijoux</h2>
             <p className="text-sm opacity-80">Handmade jewelry</p>
           </Link>
